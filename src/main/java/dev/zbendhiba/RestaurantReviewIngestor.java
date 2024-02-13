cb0be8a97f1d1a828f58628245a46111dd66bdcc
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2024-02-13 15:10:35 +0100

    Add Camel

---
+++ src/main/java/dev/zbendhiba/RestaurantReviewIngestor.java
@@ -0,0 +1,10 @@
+package dev.zbendhiba;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class RestaurantReviewIngestor extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
