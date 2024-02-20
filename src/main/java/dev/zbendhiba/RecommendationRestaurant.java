d7dea2b9ec7b231c61a74f9e1c3a424d71954f2b
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2024-02-20 21:53:25 +0100

    prompt

--- src/main/java/dev/zbendhiba/RecommendationRestaurant.java
+++ src/main/java/dev/zbendhiba/RecommendationRestaurant.java
@@ -11,29 +11,27 @@ public interface RecommendationRestaurant {
 
     @SystemMessage("""
           
-         In this task, your responses should be based solely on the data provided. Do not consider any other sources of information. The restaurant are located in Blabla city.
+         In this task, your responses should be based solely on the data we provide you. Do not consider any other sources of information. The restaurant are located in Blabla city.
                                                                                                                                                                                          
          Scenario:
-         You are asked to provide recommendations for a restaurant based on user reviews. Use the information retrieved by the data to generate responses that accurately reflect the sentiment and content of the retrieved reviews. Ensure that your responses are relevant and helpful to the user, taking into account the specific preferences and requirements mentioned in the reviews.
+         You are asked to provide recommendations for a restaurant based on user reviews. The reviews are information we do provide you, and you can use only the data we provide you.
                                                                                                                                                                                          
          Instructions:
-         1. Only use the reviews in the data provided.
+         1. Only use the reviews in the data we provide you.
          2. Do not incorporate any external knowledge or additional sources of information.
-         3. Focus on providing high-quality recommendations that align with the user's needs and preferences as inferred from the retrieved reviews.
-         4. If you don't find any restaurant in data, don't give any recommendation.
+         3. If you don't find any restaurant from the data we provide you, be honest, don't give any recommendation.
                                                                                                                                                                                          
-        Remember: Your goal is to leverage the information provided by data to deliver accurate and relevant responses to the user's queries.
+        Remember: Your goal is to leverage the information provided by our data to deliver accurate and relevant responses to the user's queries.
                                                                                                                                                                                          
             """)
     @UserMessage("""
             
             For example:
-            - "I want the best Restaurant to eat American".
             - "I want recommendation for a restaurant with Chineese food"
 
              Answer:
             - Based on reviews we have from our users, I recommend 1041 Pizza for American restaurant at Blabla city.
-            - Based on reviews in my database, I didn't find any specific American restaurant to recommend. Feel free to try one and send your reviews.
+            - Based on the available data, I didn't find any specific American restaurant to recommend. Feel free to try one and send your reviews.
 
             ---
             {question}
--- src/main/java/dev/zbendhiba/RestaurantReviewIngestorBean.java
+++ src/main/java/dev/zbendhiba/RestaurantReviewIngestorBean.java
@@ -14,7 +14,7 @@ public class RestaurantReviewIngestorBean {
 
     public Document createDocument(RestaurantReview restaurantReview){
         // Create description Document from Exchange properties
-        String text = "Restaurant name is : " + restaurantReview.getName() +", type of cuisine is : "+ restaurantReview.getType() + ", review from a YesRestaurant user : "+ restaurantReview.getReview() + ", is located in Blabla city";
+        String text = "Restaurant name is : " + restaurantReview.getName() +", type of cuisine is : "+ restaurantReview.getType() + ", review from a user : "+ restaurantReview.getReview() + ", is located in Blabla city";
         Map<String, String> map = new HashMap<>();
         map.put("restaurantName", restaurantReview.getName());
         map.put("cusineType", restaurantReview.getType());
