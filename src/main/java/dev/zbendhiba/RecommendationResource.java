9497a933b80ba88377a35978d41484aaa1d42b6a
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2024-02-21 14:32:33 +0100

    end

--- src/main/java/dev/zbendhiba/RecommendationResource.java
+++ src/main/java/dev/zbendhiba/RecommendationResource.java
@@ -2,7 +2,6 @@ package dev.zbendhiba;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 @ApplicationScoped
